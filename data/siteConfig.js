4ec3e0a3d094403523f7a902eed0c0c254fad14c
Pedro Mello <pedro.mello@riachuelo.com.br>
2021-04-12 17:34:08 -0300

    feat: redirect to medium blog

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -19,61 +19,68 @@ module.exports = {
   skills: [
     {
       name: 'HTML/CSS',
-      level: 60
+      level: 60,
     },
     {
       name: 'Javascript',
-      level: 70
+      level: 70,
     },
     {
       name: 'React',
-      level: 65
+      level: 65,
     },
     {
       name: 'Redux',
-      level: 45
+      level: 45,
     },
     {
       name: 'React Native',
-      level: 60
+      level: 60,
     },
     {
       name: 'Node',
-      level: 45
+      level: 45,
     },
     {
       name: 'Flutter',
-      level: 30
+      level: 30,
     },
     {
       name: 'Design de Interfaces',
-      level: 50
+      level: 50,
     },
   ],
   jobs: [
     {
-      title: "Desenvolvimento",
+      title: 'Desenvolvimento',
       type: 'coding',
-      description: "Mão na massa! Programar é minha paixão, então vou tirar sua ideia do papel (e planilhas) e tornar ela realidade. Utilizando as tecnologias de ponta do mercado, podemos ir desde uma landing page para seu produto, site pessoal, blog com CMS, ou aplicativo para smartphone."
-    },  {
-      title: "Design de Interfaces",
+      description:
+        'Mão na massa! Programar é minha paixão, então vou tirar sua ideia do papel (e planilhas) e tornar ela realidade. Utilizando as tecnologias de ponta do mercado, podemos ir desde uma landing page para seu produto, site pessoal, blog com CMS, ou aplicativo para smartphone.',
+    },
+    {
+      title: 'Design de Interfaces',
       type: 'design',
-      description: "Qual o sentido de uma ideia que não agrega valor? Posso te ajudar com protótipos de baixa e alta fidelidade, utilizando softwares modernos para a construção e validação em tempo real."
-    }, {
-      title: "Consultoria (SELO ANTI-COACH®)",
+      description:
+        'Qual o sentido de uma ideia que não agrega valor? Posso te ajudar com protótipos de baixa e alta fidelidade, utilizando softwares modernos para a construção e validação em tempo real.',
+    },
+    {
+      title: 'Consultoria (SELO ANTI-COACH®)',
       type: 'consultant',
-      description: "Não sou palestrinha, mas gosto muito de ajudar (principalmente quem tá começando). Pode me chamar pra falar na faculdade, evento de tecnologia, evento de empresa, evento informal (cerveja por sua conta). Também pode contar comigo para auxiliar a traçar um plano, entender o mercado crescente de tecnologia, aprender mais sobre determinada tecnologia ou colocar a internet para trabalhar para você. Afinal, se sua empresa/marca não está na internet, onde mais ela está?"
-    }, {
-      title: "Mentoria 1-1",
+      description:
+        'Não sou palestrinha, mas gosto muito de ajudar (principalmente quem tá começando). Pode me chamar pra falar na faculdade, evento de tecnologia, evento de empresa, evento informal (cerveja por sua conta). Também pode contar comigo para auxiliar a traçar um plano, entender o mercado crescente de tecnologia, aprender mais sobre determinada tecnologia ou colocar a internet para trabalhar para você. Afinal, se sua empresa/marca não está na internet, onde mais ela está?',
+    },
+    {
+      title: 'Mentoria 1-1',
       type: 'mentor',
-      description: "Ao longo da carreira, passei pelos mais diversos tipos de empresas (startup, consultoria, fábrica de software, agência de marketing, banco e varejo) e trabalhei em produtos ainda mais diversificados. O que aprendi, pode ser o suficiente para você descobrir as coisas que gosta e que não gosta. Vamos desde os fundamentos e pilares do desenvolvimento web, até o desenvolvimento de projetos para seu portfólio. É uma oportunidade praticamente exclusiva, pois só faço a mentoria com uma pessoa por vez. Mas se tiver interesse, entre em contato para eu ajudar da melhor forma que estiver ao meu alcance."
+      description:
+        'Ao longo da carreira, passei pelos mais diversos tipos de empresas (startup, consultoria, fábrica de software, agência de marketing, banco e varejo) e trabalhei em produtos ainda mais diversificados. O que aprendi, pode ser o suficiente para você descobrir as coisas que gosta e que não gosta. Vamos desde os fundamentos e pilares do desenvolvimento web, até o desenvolvimento de projetos para seu portfólio. É uma oportunidade praticamente exclusiva, pois só faço a mentoria com uma pessoa por vez. Mas se tiver interesse, entre em contato para eu ajudar da melhor forma que estiver ao meu alcance.',
     },
   ],
   social: {
-    linkedin: "https://www.linkedin.com/in/pedro-c-mello",
-    github: "https://github.com/pedrocmello",
-    email: "contato@cicatriz.dev",
-    whatsapp: "5532999255105"
+    linkedin: 'https://www.linkedin.com/in/pedro-c-mello',
+    github: 'https://github.com/pedrocmello',
+    email: 'contato@cicatriz.dev',
+    whatsapp: '5532999255105',
   },
   siteUrl: 'https://cicatriz.dev',
   pathPrefix: '/',
@@ -81,7 +88,7 @@ module.exports = {
   googleAnalyticsId: 'G-V07973F3M6',
   background_color: '#ffffff',
   theme_color: '#320335',
-  fontColor: "#000000cc",
+  fontColor: '#000000cc',
   enableDarkmode: false,
   display: 'minimal-ui',
   icon: 'src/assets/gatsby-icon.png',
@@ -91,8 +98,8 @@ module.exports = {
       url: '/',
     },
     {
-      label: 'Blog',
-      url: 'https://blog.cicatriz.dev',
-    }
-  ]
+      label: 'Medium',
+      url: 'https://medium.com/@cicatrizdev',
+    },
+  ],
 }
